7202b1519fe7d5fa807344666bb33223c342cb20
Bhargavi Gangoor <154064565+BhargaviGangoor@users.noreply.github.com>
2026-01-25 09:07:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_db_connection():
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
+            port=int(os.getenv("DB_PORT")),
             database=os.getenv("DB_NAME"),
             sslmode="require"  # This is the safest way to pass sslmode
         )
